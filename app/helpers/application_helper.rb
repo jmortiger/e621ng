0b2983e7b7c4c73014c556d9f97135923f24cd95
Justin Morris <jmortiger@gmail.com>
2025-12-19 12:48:19 +0000

    Pass for Rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -96,8 +96,8 @@ module ApplicationHelper
   def error_messages_for(instance_name)
     instance = instance_variable_get("@#{instance_name}")
 
-    if instance && instance.errors.any?
-      %{<div class="error-messages ui-state-error ui-corner-all"><strong>Error</strong>: #{instance.__send__(:errors).full_messages.join(", ")}</div>}.html_safe
+    if instance&.errors&.any?
+      %(<div class="error-messages ui-state-error ui-corner-all"><strong>Error</strong>: #{instance.__send__(:errors).full_messages.join(', ')}</div>).html_safe
     else
       ""
     end
@@ -110,11 +110,11 @@ module ApplicationHelper
 
   def time_ago_in_words_tagged(time, compact: false)
     if time.past?
-      text = time_ago_in_words(time) + " ago"
+      text = "#{time_ago_in_words(time)} ago"
       text = text.gsub(/almost|about|over/, "") if compact
       raw time_tag(text, time)
     else
-      raw time_tag("in " + distance_of_time_in_words(Time.now, time), time)
+      raw time_tag("in #{distance_of_time_in_words(Time.now, time)}", time)
     end
   end
 
@@ -128,19 +128,19 @@ module ApplicationHelper
 
   def external_link_to(url, truncate: nil, strip_scheme: false, link_options: {})
     text = url
-    text = text.gsub(%r!\Ahttps?://!i, "") if strip_scheme
+    text = text.gsub(%r{\Ahttps?://}i, "") if strip_scheme
     text = text.truncate(truncate) if truncate
 
-    if url =~ %r!\Ahttps?://!i
-      link_to text, url, {rel: :nofollow}.merge(link_options)
+    if url =~ %r{\Ahttps?://}i
+      link_to text, url, { rel: :nofollow }.merge(link_options)
     else
       url
     end
   end
 
   def link_to_ip(ip)
-    return '(none)' unless ip
-    link_to ip, moderator_ip_addrs_path(:search => {:ip_addr => ip})
+    return "(none)" unless ip
+    link_to ip, moderator_ip_addrs_path(search: { ip_addr: ip })
   end
 
   def link_to_user(user, include_activation: false)
@@ -176,12 +176,12 @@ module ApplicationHelper
   end
 
   def data_attributes_for(record, prefix, attributes)
-    attributes.map do |attr|
+    attributes.to_h do |attr|
       name = attr.to_s.dasherize.delete("?")
       value = record.send(attr)
 
       [:"#{prefix}-#{name}", value]
-    end.to_h
+    end
   end
 
   def user_avatar(user)
@@ -302,52 +302,49 @@ module ApplicationHelper
     return url == request.path if controller == "static"
 
     url =~ case controller
-    when "sessions", "users", "maintenance/user/login_reminders", "maintenance/user/password_resets", "admin/users", "dmails"
-      /^\/(session|users)/
+           when "sessions", "users", "maintenance/user/login_reminders", "maintenance/user/password_resets", "admin/users", "dmails"
+             %r{^/(session|users)}
 
-    when "post_sets"
-      /^\/post_sets/
+           when "post_sets"
+             %r{^/post_sets}
 
-    when "blips"
-      /^\/blips/
+           when "blips"
+             %r{^/blips}
 
-    when "forum_posts"
-      /^\/forum_topics/
+           when "forum_topics", "forum_posts"
+             %r{^/forum_topics}
 
-    when "comments"
-      /^\/comments/
+           when "comments"
+             %r{^/comments}
 
-    when "notes", "note_versions"
-      /^\/notes/
+           when "notes", "note_versions"
+             %r{^/notes}
 
-    when "posts", "uploads", "post_versions", "popular", "moderator/post/dashboards", "favorites", "post_favorites"
-      /^\/posts/
+           when "posts", "uploads", "post_versions", "popular", "moderator/post/dashboards", "favorites", "post_favorites"
+             %r{^/posts}
 
-    when "artists", "artist_versions"
-      /^\/artist/
+           when "artists", "artist_versions"
+             %r{^/artist}
 
-    when "tags", "meta_searches", "tag_aliases", "tag_alias_requests", "tag_implications", "tag_implication_requests", "related_tags"
-      /^\/tags/
+           when "tags", "meta_searches", "tag_aliases", "tag_alias_requests", "tag_implications", "tag_implication_requests", "related_tags"
+             %r{^/tags}
 
-    when "pools", "pool_versions"
-      /^\/pools/
+           when "pools", "pool_versions"
+             %r{^/pools}
 
-    when "moderator/dashboards"
-      /^\/moderator/
+           when "moderator/dashboards"
+             %r{^/moderator}
 
-    when "wiki_pages", "wiki_page_versions"
-      /^\/wiki_pages/
+           when "wiki_pages", "wiki_page_versions"
+             %r{^/wiki_pages}
 
-    when "forum_topics", "forum_posts"
-      /^\/forum_topics/
+           when "help"
+             %r{^/help}
 
-    when "help"
-      /^\/help/
-
-    # If there is no match activate the site map only
-    else
-      /^#{site_map_path}/
-    end
+           # If there is no match activate the site map only
+           else
+             /^#{site_map_path}/
+           end
   end
 
   private
